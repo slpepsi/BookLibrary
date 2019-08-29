945efc72987e24f4955ce3d455d9a765e33f2f5c
sl <1285988560@qq.com>
2019-08-29 13:43:08 +0800

    edit gunicorn.config

---
+++ gunicorn_conf.py
@@ -0,0 +1,22 @@
+# encoding: utf-8
+# gunicorn.conf
+
+# 并行工作进程数
+workers = 2
+# 指定每个工作者的线程数
+threads = 2
+# 监听内网端口5000
+bind = '0.0.0.0:5050'
+# 设置守护进程,将进程交给supervisor管理
+daemon = 'false'
+# 工作模式协程
+# worker_class = 'gevent'
+# 设置最大并发量
+worker_connections = 2000
+# 设置进程文件目录
+pidfile = '/Users/wulian/Documents/wlcode/tmp/gunicorn.pid'
+# 设置访问日志和错误信息日志路径
+accesslog = '/Users/wulian/Documents/wlcode/tmp/gunicorn_acess.log'
+errorlog = '/Users/wulian/Documents/wlcode/tmp/gunicorn_error.log'
+# 设置日志记录水平
+loglevel = 'debug'
